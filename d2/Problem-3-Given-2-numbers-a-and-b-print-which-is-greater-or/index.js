fbf25b829dc12ca82c54dc37375d0a516ca1d353
MZishan91 <zishan.ut@gmail.com>
2022-09-07 13:56:10 +0530

    Mathametical Oparetors

---
+++ d2/Problem-3-Given-2-numbers-a-and-b-print-which-is-greater-or/index.js
@@ -0,0 +1,9 @@
+// Problem 3: Given 2 numbers a and b print which is great or "both equal".
+let a=90;b=2;
+if(a>b){
+  console.log(a," is greaterthen",b);
+}else if(a==b){
+  console.log("both equal");
+}else{
+  console.log(b," is graterthen" ,a)
+}
