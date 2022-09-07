76958eda7110d6897d9b444f0678d62eb50ddefc
MZishan91 <zishan.ut@gmail.com>
2022-09-07 14:21:20 +0530

    day 3 Logical Operators (IW) CODING  3 Sep, 22

---
+++ d3/Problem-2-Given-any-character-if-it-is-a-vowel-print-Vow/index.js
@@ -0,0 +1,8 @@
+// Problem 2 : Given any character, if it is a vowel print "Vowel"
+let char="a";
+char=="a" ? console.log("Vowel")
+  : char=="e"? console.log("Vowel")
+   : char=="i" ? console.log("Vowel")
+     : char=="o" ? console.log("Vowel")
+      : char=="u" ?  console.log("Vowel")
+        : console.log("Cosonents");
---
+++ d3/Problem-4-Given-3-numbers-all-different-values-print-whi/index.js
@@ -0,0 +1,7 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+let a=10;
+let b=20;
+let c=30;
+a>b && a>c ? console.log(a,"is Greater")
+  : b>a && b>c ? console.log(b,"is Greater")
+  : console.log(c,"is Greater");
